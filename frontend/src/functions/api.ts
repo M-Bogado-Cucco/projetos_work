4304531672930e75d7db00912a4124379578c0ec
Marcelo Bogado <celombc@gmail.com>
2025-03-23 19:36:59 -0300

    final pj portfolio-cod3r gitignore c

--- frontend/src/functions/api.ts
+++ frontend/src/functions/api.ts
@@ -1,27 +1,13 @@
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
+const baseURL = process.env.NEXT_PUBLIC_API_URL
 
 export async function httpGet(url: string) {
-    // Garante que a URL seja normalizada antes de fazer a requisição
-    const normalizedUrl = normalize(`${baseURL}/${url}`);
-    console.log(normalizedUrl);
-
-    try {
-        const response = await fetch(normalizedUrl);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        throw error;
-    }
+	console.log(normalizarUrl(`${baseURL}/${url}`))
+	const response = await fetch(normalizarUrl(`${baseURL}/${url}`))
+	return response.json()
 }
 
-function normalize(url: string): string {
-    if (!url) {
-        throw new Error("Invalid URL provided");
-    }
-
-    // Substitui múltiplas barras consecutivas por uma única barra usando replace
-    return url.replace(/([^:]\/)\/+/g, "$1");
+function normalizarUrl(url: string) {
+	const protocolo = url.split("://")[0]
+	const restante = url.split("://")[1]
+	return `${protocolo}://${restante.replaceAll(/\/{2,}/g, "/")}`
 }
